ae7a4d4229d640b97c501ed35dadf43713282ba1
rafa_99 <raroma09@gmail.com>
2021-09-18 00:30:59 +0100

    Found New Faster Algorithm

--- include/utils.h
+++ include/utils.h
@@ -1,9 +1,6 @@
 #pragma once
 #include "video.h"
 
-int tokenCount (char *string, char *delimiter);
-char **tokenizer (char *string, char *delimiter);
-void freeTokens (char **tokens, int size);
 char *queryNormalizer (char *query);
 char *extractQueryJSON (char *youtubeurl);
 int checkNumber (char *num);
